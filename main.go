5f5cd8fc7160cc228da504f285dc0d5cb9dd8d29
ElliottCepin <elliottcepin5@gmail.com>
2026-01-18 22:58:45 -0700

    added to steps

--- main.go
+++ main.go
@@ -19,5 +19,7 @@ func serveUpload(w http.ResponseWriter, r *http.Request) {
 	// please write a blog post about io.Reader, mime/multipart's multipart.File, allocating memory in go,
 	// and whatever else you need to be able to read a file and write it to disk
 	// thx bestie
+	// do remember that you don't have to do allocation, you can get a byteslice from io.ReadAll(file) ([]byte, err).
+
 
 }
