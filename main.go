fc5e214e893ec22a30b854c0bcebe4ac842d42f5
ElliottCepin <elliottcepin5@gmail.com>
2026-01-18 22:32:52 -0700

    added steps for figuring this out

--- main.go
+++ main.go
@@ -9,4 +9,15 @@ import (
 func serveUpload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(10 * 1000 * 1000) // 10b * 1000 = 10Kb; 10Kb * 1000 = 10Mb
 	
+	file, err := r.MultipartForm.Get("file").Open
+	if (err != nil) {
+		// do something
+	}
+
+	// to actually get something out of file, you have to do file.Read(buffer []byte),
+	// but you don't really understand any of that nonsense
+	// please write a blog post about io.Reader, mime/multipart's multipart.File, allocating memory in go,
+	// and whatever else you need to be able to read a file and write it to disk
+	// thx bestie
+
 }
