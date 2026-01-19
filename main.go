bdea3aa49de502a410d0f37c9ddb8f1125975673
ElliottCepin <elliottcepin5@gmail.com>
2026-01-19 10:31:44 -0700

    added basic data flow - errors left unhandled

--- main.go
+++ main.go
@@ -3,16 +3,59 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 
 func serveUpload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(10 * 1000 * 1000) // 10b * 1000 = 10Kb; 10Kb * 1000 = 10Mb
 	
-	file, err := r.MultipartForm.Get("file").Open
+	header, err := r.MultipartForm.File.Get("file")
 	if (err != nil) {
 		// do something
 	}
+
+	name, err := r.MultipartForm.Value.Get("file")
+
+	if (err != nil) {
+		// do something
+	}
+
+	file, err := header.Open()
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+
+	if (err != nil) {
+		// do something
+	}
+
+	path, err := os.Getwd()
+
+	if (err != nil) {
+		// do something
+	}
+
+	dir := filepath.Join(path, "/upload/" + time.Now() + "_" + name)
+
+	f, err := os.Create(dir)
+
+	if (err != nil) {
+		// do something
+	}
+	
+	defer f.Close()
+
+	n, err := f.Write(data)
+
+	if (err != nil) {
+		// do something
+	}
+
 
 	// to actually get something out of file, you have to do file.Read(buffer []byte),
 	// but you don't really understand any of that nonsense
