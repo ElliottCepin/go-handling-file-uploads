9b20d35089599ffbf4efabc9b549ca426fe78024
ElliottCepin <elliottcepin5@gmail.com>
2026-01-28 15:49:10 -0700

    file upload working

--- main.go
+++ main.go
@@ -13,56 +13,62 @@ import (
 func serveUpload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(10 * 1000 * 1000) // 10b * 1000 = 10Kb; 10Kb * 1000 = 10Mb
 	
-	header, err := r.MultipartForm.File.Get("file")
-	if (err != nil) {
-		// do something
+	header := r.MultipartForm.File["file"]
+	if (header[0] == nil) {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Bad request\n")
+		return
 	}
 
-	name, err := r.MultipartForm.Value.Get("file")
+	file, err := header[0].Open()
 
 	if (err != nil) {
-		// do something
+		w.WriteHeader(422)
+		fmt.Fprint(w, "Unprossessable entity\n")
+		return
 	}
-
-	file, err := header.Open()
 
 	defer file.Close()
 
 	data, err := io.ReadAll(file)
 
 	if (err != nil) {
-		// do something
+		w.WriteHeader(422)
+		fmt.Fprint(w, "Unprossessable entity\n")
+		return
 	}
 
 	path, err := os.Getwd()
 
 	if (err != nil) {
-		// do something
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Internal server error - os.Getwd\n")
+		return
 	}
 
-	dir := filepath.Join(path, "/upload/" + time.Now() + "_" + name)
+	dir := filepath.Join(path, "/upload/" + time.Now().String()[0:20] + ".file")
 
 	f, err := os.Create(dir)
 
 	if (err != nil) {
-		// do something
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Internal server error - os.Create\n%w\n", err)
+		return
 	}
 	
 	defer f.Close()
 
-	n, err := f.Write(data)
+	_, err = f.Write(data)
 
 	if (err != nil) {
-		// do something
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Internal server error")
+		return
 	}
+}
 
+func main() {
+	http.HandleFunc("/upload", serveUpload)
 
-	// to actually get something out of file, you have to do file.Read(buffer []byte),
-	// but you don't really understand any of that nonsense
-	// please write a blog post about io.Reader, mime/multipart's multipart.File, allocating memory in go,
-	// and whatever else you need to be able to read a file and write it to disk
-	// thx bestie
-	// do remember that you don't have to do allocation, you can get a byteslice from io.ReadAll(file) ([]byte, err).
-
-
+	http.ListenAndServe(":8080", nil)
 }
